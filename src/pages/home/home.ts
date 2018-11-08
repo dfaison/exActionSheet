6aece910d96e9a36fe3966c4d9ddb9c494233c19
faison <dfiason@ptnicec.ac.th>
2018-11-08 14:28:24 +0700

    send project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
+import { errorHandler } from '@angular/platform-browser/src/browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public acSheet: ActionSheetController,
+    public navCtrl: NavController) {
 
+  }
+
+  showAction(){
+    const action =this.acSheet.create({
+      title: 'เลือกรายการ',
+      subTitle: '----------',
+      buttons: [
+        {
+          text: 'App'
+        },
+        {
+          text: 'Edit',
+          role: 'edit',
+          handler: () =>{
+            console.log('แก้ไขแล้ว');
+          }
+        },
+        {
+          text: 'Delete',
+          role: 'delete',
+          handler: () =>{
+            console.log('ลบเรียบร้อยแล้ว');
+          }
+        }
+      ]
+    });
+    action.present();
   }
 
 }
